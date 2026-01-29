b3c1c59f118cb14461efe1bb66d08926255bcab5
Tiendat2K2 <nguyentiendat2k34@gmail.com>
2026-01-29 14:33:46 +0700

    up de

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -37,7 +37,7 @@ const NotFoundPage = () => {
             ></dotlottie-wc>
           ) : (
             <div style={{ width: "350px", height: "350px", display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '80px' }}>
-              ❌
+              Trang không tồn tại
             </div>
           )}
         </div>
